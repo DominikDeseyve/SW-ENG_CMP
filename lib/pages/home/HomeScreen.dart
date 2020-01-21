5ea0e2bf9513bf8d6b914886eed4ad2f729adfb6
Robler25 <robin.wangen@gmail.com>
2020-01-21 17:39:55 +0100

    Startseite erstellt aber noch nicht Hintergrund eingefügt

--- lib/pages/home/HomeScreen.dart
+++ lib/pages/home/HomeScreen.dart
@@ -7,9 +7,108 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("home"),
-      ),
-    );
+        body: Container(
+            margin: const EdgeInsets.fromLTRB(40.0, 60.0, 40.0, 60.0),
+            child: ListView(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Willkommen",
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    Text(
+                      "bei",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    Text(
+                      "CMP",
+                      style: TextStyle(
+                        fontSize: 60.0,
+                        color: Colors.redAccent,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(40.0, 80.0, 40.0, 50.0),
+                  height: 50.0,
+                  child: FlatButton(
+                    onPressed: () {},
+                    color: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.search,
+                          size: 28.0,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Playlist suchen",
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Divider(
+                    thickness: 2.5,
+                    indent: 10.0,
+                    endIndent: 10.0,
+                    color: Colors.black87,
+                  )),
+                  Text("ODER",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black87,
+                      )),
+                  Expanded(
+                      child: Divider(
+                    thickness: 2.5,
+                    indent: 10.0,
+                    endIndent: 10.0,
+                    color: Colors.black87,
+                  )),
+                ]),
+                Container(
+                  margin: EdgeInsets.fromLTRB(40.0, 50.0, 40.0, 0.0),
+                  height: 50.0,
+                  child: FlatButton(
+                    onPressed: () {},
+                    color: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.add,
+                          size: 28.0,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "Playlist erstellen",
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )));
   }
 }
