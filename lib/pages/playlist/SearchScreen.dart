492ace00ca5d50e51e6d77bbe01c48148f3269d4
flobe99 <60112645+flobe99@users.noreply.github.com>
2020-02-12 23:56:38 +0100

    Search Playlistname
    Durch die Eingabe in TextField werden alle bereits eingetragenen angezeigt

--- lib/pages/playlist/SearchScreen.dart
+++ lib/pages/playlist/SearchScreen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cmp/logic/Firebase.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -5,27 +7,46 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  String _playlistName;
-  String _typ;
-  String _artist;
+  var queryResultSet = [];
+  var tempSearchStore = [];
 
-  void initState() {
-    super.initState();
-    this._playlistName = "Platte";
-    this._typ = "Album";
-    this._artist = "Apache 207";
+  initiateSearch(value) {
+    if (value.length == 0) {
+      setState(() {
+        queryResultSet = [];
+        tempSearchStore = [];
+      });
+    }
+
+    var capitalizedValue =
+        value.substring(0, 1).toUpperCase() + value.substring(1);
+
+    if (queryResultSet.length == 0 && value.length == 1) {
+      SearchService().searchByName(value).then((QuerySnapshot docs) {
+        for (int i = 0; i < docs.documents.length; ++i) {
+          queryResultSet.add(docs.documents[i].data);
+        }
+      });
+    } else {
+      tempSearchStore = [];
+      queryResultSet.forEach((element) {
+        if (element['name'].startsWith(capitalizedValue)) {
+          setState(() {
+            tempSearchStore.add(element);
+          });
+        }
+      });
+    }
   }
 
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        margin: EdgeInsets.only(top: 50.0),
-        alignment: Alignment.center,
-        child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
+    return new Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+            margin: EdgeInsets.only(top: 50.0),
+            alignment: Alignment.center,
+            child: Column(children: <Widget>[
               Container(
                 child: Text(
                   "Suchen",
@@ -35,13 +56,18 @@ class _SearchScreenState extends State<SearchScreen> {
               ),
               Container(
                 alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 50.0),
+                margin: EdgeInsets.only(top: 50.0, bottom: 20.0),
                 width: MediaQuery.of(context).size.width * 0.8,
-                decoration: BoxDecoration(color: Colors.redAccent, borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                decoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(30.0))),
                 child: TextField(
-                  style: TextStyle(color: Colors.white, decorationColor: Colors.white),
+                  onChanged: (val) {
+                    initiateSearch(val);
+                  },
+                  style: TextStyle(
+                      color: Colors.white, decorationColor: Colors.white),
                   autocorrect: false,
-                  //textAlign: TextAlign.center,
                   decoration: InputDecoration(
                     prefixIcon: Icon(Icons.search, color: Colors.white),
                     hintText: "Playlist eingeben",
@@ -51,39 +77,52 @@ class _SearchScreenState extends State<SearchScreen> {
                   ),
                 ),
               ),
-              Container(
-                child: Expanded(
-                  child: ListView.builder(
-                    itemCount: 20,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: EdgeInsets.all(10.0),
-                        //color: Color(0xFF253A4B),
-                        child: ListTile(
-                          leading:
-                              Container(width: 60.0, height: 60.0, margin: EdgeInsets.all(1), decoration: BoxDecoration(shape: BoxShape.circle, image: DecorationImage(fit: BoxFit.cover, image: AssetImage('assets/images/playlist.jpg')))),
-                          title: Container(
-                            child: Text(
-                              _playlistName,
-                              style: TextStyle(color: Color(0xFF253A4B), fontSize: 20.0),
-                            ),
-                          ),
-                          subtitle: Text(
-                            _typ + " - " + _artist,
-                            style: TextStyle(color: Color(0xFF253A4B)),
-                          ),
-                          trailing: Icon(
-                            Icons.more_vert,
-                            color: Color(0xFF253A4B),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              )
-            ]),
+              ListView(
+                  padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                  primary: false,
+                  shrinkWrap: true,
+                  children: tempSearchStore.map((element) {
+                    return builtItems(element);
+                  }).toList()),
+            ])));
+  }
+}
+
+Widget builtItems(data) {
+  return Container(
+    child: ListTile(
+      leading: Container(
+          width: 60.0,
+          height: 60.0,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/playlist.jpg')))),
+      title: Container(
+        child: Text(
+          data['name'],
+          style: TextStyle(color: Color(0xFF253A4B), fontSize: 20.0),
+        ),
       ),
-    );
+      subtitle: Text(
+        "Apache",
+        style: TextStyle(color: Color(0xFF253A4B)),
+      ),
+      trailing: Icon(
+        Icons.more_vert,
+        color: Color(0xFF253A4B),
+      ),
+    ),
+  );
+}
+
+class SearchService {
+  searchByName(String searchField) {
+    return Firestore.instance
+        .collection('playlist')
+        .where('searchKey',
+            isEqualTo: searchField.substring(0, 1).toUpperCase())
+        .getDocuments();
   }
 }
